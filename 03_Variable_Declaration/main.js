5c16d043a88f1f44d4e95bfe844a63e876488e07
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-10 22:44:50 +0200

    Variable declaration

---
+++ 03_Variable_Declaration/main.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+console.log("-------");
+/*--------------------------------------------------- */
+var message = "Hello World !";
+console.log(message);
+/* variables declaration
+=========================*/
+var x = 10;
+var y = 20;
---
+++ 03_Variable_Declaration/main.ts
@@ -0,0 +1,17 @@
+/*
+ * writing this line help typescript consider
+ * this file as a module and not a script
+ */
+export {};
+console.log("-------");
+/*--------------------------------------------------- */
+
+let message = "Hello World !";
+console.log(message);
+
+/* variables declaration 
+=========================*/
+let x = 10;
+const y = 20;
+
+
