3ac068f15b1db5d1019dc15879d09bd113247b08
danielOuattara <daniel.ouattara@gmx.com>
2023-02-20 21:08:29 +0100

    update file + correct

--- 02_Environment_Setup/main.js
+++ 02_Environment_Setup/main.js
@@ -1,6 +1,26 @@
 "use strict";
 exports.__esModule = true;
-console.log("-------");
 /*--------------------------------------------------- */
 var message = "Hello World !";
-console.log(message); 
+console.log(message);
+/* variables declaration
+=========================*/
+var x = 10;
+var myGender = "male";
+var numbers = 12;
+var sum;
+var sum2;
+var title = "CodeEvolution";
+var isBeginner = true; // declaration & initialization
+var total; // declaration,
+total = 0; // --> intellisense of total: total.something
+//----------------------------------
+var name = "Danny Boy";
+var sentence = "".concat(name, " started learning TypeScript this month");
+console.log(sentence);
+console.log("-------");
+// ---------------------------------
+var n = null;
+var undef = undefined;
+// let isBool: boolean = null; // Incorrect  bool is not null
+// let myName: string = undefined; //  incorrect: string is  undefined
--- 02_Environment_Setup/main.ts
+++ 02_Environment_Setup/main.ts
@@ -1,9 +1,9 @@
 /*
- * writing this line help typescript consider
- * this file as a module and not a script
+ * writing this lines below help typescript
+ * consider this file as a module and not a
+ * script
  */
 export {};
-console.log("-------");
 /*--------------------------------------------------- */
 
 let message = "Hello World !";
@@ -12,21 +12,22 @@ console.log(message);
 /* variables declaration 
 =========================*/
 let x = 10;
-const y = 20;
+const myGender = "male";
 
 let numbers: number = 12;
 
 let sum;
+
 let sum2: number;
 const title = "CodeEvolution";
 
-let isBeginner: boolean = true; // declaration & initialisation
+let isBeginner: boolean = true; // declaration & initialization
 let total: number; // declaration,
 total = 0; // --> intellisense of total: total.something
 
 //----------------------------------
 let name: string = "Danny Boy";
-let sentence: string = `${name} started learning Typscript this month`;
+let sentence: string = `${name} started learning TypeScript this month`;
 console.log(sentence);
 
 console.log("-------");
@@ -34,5 +35,5 @@ console.log("-------");
 let n: null = null;
 let undef: undefined = undefined;
 
-let isBool: boolean = null;
-let myName: string = undefined;
+// let isBool: boolean = null; // Incorrect  bool is not null
+// let myName: string = undefined; //  incorrect: string is  undefined
